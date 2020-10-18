85e6cbe9ddee2698c29be47a8d0ec11d0549e3ea
Peter Senn <psenn@vertafore.com>
2020-10-18 17:40:29 -0600

    adjust for tasty recipe, and avant garde

--- popup.js
+++ popup.js
@@ -20,8 +20,8 @@ document.getElementById("getSource").addEventListener('click', () => {
         let instructions 
         recipe_selectors.every(s => {
             let recipeContainerInDOM = document.querySelector(s);
-
-            //word press
+            
+            //word press --- (minalmist baker, hot for food)
             if (recipeContainerInDOM && s === '.wprm-recipe-container'){
                 // recipe content found, set notes, ingredients, and instructions
                 notes = recipeContainerInDOM.querySelector('.wprm-recipe-notes-container')
@@ -32,7 +32,29 @@ document.getElementById("getSource").addEventListener('click', () => {
                 return false;
             }
 
-            //TODO - set recipe data from other recipe sites
+            //tasty recipes --- (choosing chia)
+            if (recipeContainerInDOM && s === '.tasty-recipe'){
+                // recipe content found, set notes, ingredients, and instructions
+                // notes = recipeContainerInDOM.querySelector('.wprm-recipe-notes-container')
+                // ingredients = recipeContainerInDOM.querySelectorAll('.wprm-recipe-ingredient-group')
+                // instructions = recipeContainerInDOM.querySelectorAll('.wprm-recipe-instruction-group')
+
+                // recipe content found, stop iterating through recipe_selectors
+                return false;
+            }
+
+            //recipe content(???) --- (avant garde)
+            if (recipeContainerInDOM && s === '.recipe-content'){
+                // recipe content found, set notes, ingredients, and instructions
+                // notes = recipeContainerInDOM.querySelector('.wprm-recipe-notes-container')
+                // ingredients = recipeContainerInDOM.querySelectorAll('.wprm-recipe-ingredient-group')
+                // instructions = recipeContainerInDOM.querySelectorAll('.wprm-recipe-instruction-group')
+            
+                // recipe content found, stop iterating through recipe_selectors
+                return false;
+            }
+
+            //TODO - set recipe data from more recipe sites
 
             // recipe content NOT found, keep iterating through recipe_selectors
             return true;
