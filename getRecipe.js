2f61cd37cc6afc79aec4b4413bca0927ffe4126e
Peter Senn <psenn@vertafore.com>
2020-10-23 14:30:13 -0600

    clean up, get wordpress data ready to send

--- getRecipe.js
+++ getRecipe.js
@@ -2,17 +2,19 @@ document.getElementById("getRecipe").addEventListener('click', () => {
 
     function searchDOMForRecipe() {
         const recipe_selectors = [
-            '.recipe-callout',
-            '.tasty-recipes',
-            '.easyrecipe',
-            '.innerrecipe',
-            '.recipe-summary.wide', // thepioneerwoman.com
+            //used
             '.wprm-recipe-container',
-            '.recipe-content',
-            '.simple-recipe-pro',
-            '.mv-recipe-card',
-            'div[itemtype="http://schema.org/Recipe"]',
-            'div[itemtype="https://schema.org/Recipe"]',
+            // '.recipe-content',
+            // '.tasty-recipes',
+            //unused
+            // '.recipe-callout',
+            // '.easyrecipe',
+            // '.innerrecipe',
+            // '.recipe-summary.wide', // thepioneerwoman.com
+            // '.simple-recipe-pro',
+            // '.mv-recipe-card',
+            // 'div[itemtype="http://schema.org/Recipe"]',
+            // 'div[itemtype="https://schema.org/Recipe"]',
         ]
 
         let notes
@@ -24,39 +26,48 @@ document.getElementById("getRecipe").addEventListener('click', () => {
             //word press --- (minalmist baker, hot for food)
             if (recipeContainerInDOM && s === '.wprm-recipe-container'){
                 // recipe content found, set notes, ingredients, and instructions
+                //NOTES
                 notes = recipeContainerInDOM.querySelector('.wprm-recipe-notes-container')
-                ingredients = recipeContainerInDOM.querySelectorAll('.wprm-recipe-ingredient-group')
-                instructions = recipeContainerInDOM.querySelectorAll('.wprm-recipe-instruction-group')
 
-                // recipe content found, stop iterating through recipe_selectors
-                return false;
-            }
+                //INGREDIENTS
+                const ingredientsNodes = recipeContainerInDOM.querySelectorAll('.wprm-recipe-ingredient-group')
+                ingredients = [...ingredientsNodes].map(ingredient => {
+                    return ingredient.innerText
+                })
 
-            //tasty recipes --- (choosing chia)
-            if (recipeContainerInDOM && s === '.tasty-recipes'){
-                console.log('tasty recipes');
-                console.log(recipeContainerInDOM);
+                //INSTRUCTIONS
+                const instructionsNode = recipeContainerInDOM.querySelector('.wprm-recipe-instruction-group')
+                instructions = instructionsNode.innerHTML
 
-                // recipe content found, set notes, ingredients, and instructions
-                notes = recipeContainerInDOM.querySelector('.tasty-recipes-notes')
-                ingredients = recipeContainerInDOM.querySelector('.tasty-recipes-ingredients')
-                instructions = recipeContainerInDOM.querySelector('.tasty-recipes-instructions')
-
-                // recipe content found, stop iterating through recipe_selectors
-                return false;
-            }
-
-            //recipe content(???) --- (avant garde)
-            if (recipeContainerInDOM && s === '.recipe-content'){
-                // recipe content found, set notes, ingredients, and instructions
-                ingredients = recipeContainerInDOM.querySelector('.recipe-ingredients')
-                instructions = recipeContainerInDOM.querySelector('.recipe-method')
-                
                 // recipe content found, stop iterating through recipe_selectors
                 return false;
             }
 
             //TODO - set recipe data from more recipe sites
+            //tasty recipes --- (choosing chia)
+            // if (recipeContainerInDOM && s === '.tasty-recipes'){
+            //     console.log('tasty recipes');
+            //     console.log(recipeContainerInDOM);
+
+            //     // recipe content found, set notes, ingredients, and instructions
+            //     notes = recipeContainerInDOM.querySelector('.tasty-recipes-notes')
+            //     ingredients = recipeContainerInDOM.querySelector('.tasty-recipes-ingredients')
+            //     instructions = recipeContainerInDOM.querySelector('.tasty-recipes-instructions')
+
+            //     // recipe content found, stop iterating through recipe_selectors
+            //     return false;
+            // }
+
+            // //recipe content(???) --- (avant garde)
+            // if (recipeContainerInDOM && s === '.recipe-content'){
+            //     // recipe content found, set notes, ingredients, and instructions
+            //     ingredients = recipeContainerInDOM.querySelector('.recipe-ingredients')
+            //     instructions = recipeContainerInDOM.querySelector('.recipe-method')
+
+            //     // recipe content found, stop iterating through recipe_selectors
+            //     return false;
+            // }
+
 
             // recipe content NOT found, keep iterating through recipe_selectors
             return true;
@@ -66,6 +77,7 @@ document.getElementById("getRecipe").addEventListener('click', () => {
         console.log('ingredients', ingredients);
         console.log('instructions', instructions);
 
+        //Send notes, ingredients, instructions to API
     }
 
     chrome.tabs.executeScript({
